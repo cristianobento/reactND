1a5d9c006115e949eed9973144736047e81021c9
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-02-10 14:54:25 -0200

    updating challenge 2 - lesson 3

---
+++ 003-state-management/challenges/challenge01/src/ListUsers.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ListUsers extends Component {
+  render() {
+    console.log('Props', this.props);
+    return (
+      <ol className="users-list">
+    	porra
+
+      </ol>
+    );
+  }
+}
+
+export default ListUsers;
