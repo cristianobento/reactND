d4f375a12dd3b16aa3f7dc66446dcd9a043f042d
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-02-13 00:36:01 -0200

    Adding state on ListContacts / Adding method for contact deletion

--- 003-state-management/reactnd-contacts-app/src/ListContacts.js
+++ 003-state-management/reactnd-contacts-app/src/ListContacts.js
@@ -15,7 +15,12 @@ const ListContacts = props => {
             <p>{contact.name}</p>
             <p>@{contact.handle}</p>
           </div>
-          <button className="contact-remove"> Remove </button>
+          <button
+            onClick={() => props.onDeleteContact(contact)}
+            className="contact-remove"
+          >
+            Remove
+          </button>
         </li>
       ))}
     </ol>
