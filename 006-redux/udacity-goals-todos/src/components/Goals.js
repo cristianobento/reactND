b3eb18e429bfe6434a2cd0c7c3047b9961f0b9bc
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-07 01:24:24 -0300

    bugfix: updating references and returning index.html to last usable state

--- 006-redux/udacity-goals-todos/src/components/Goals.js
+++ 006-redux/udacity-goals-todos/src/components/Goals.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { handleAddGoal, handleDeleteGoal } from "../actions/goals";
+import List from "./List";
 
 class Goals extends React.Component {
   addItem = e => {
