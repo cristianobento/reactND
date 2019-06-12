2e8b997b4bed6fe543aa0efdebe2a01ca4236211
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-12 01:15:10 -0300

    feat: adding tweet ui

--- 006-redux/reactnd-chirper-app/src/components/App.js
+++ 006-redux/reactnd-chirper-app/src/components/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { handleInitialData } from "../actions/shared";
-import Dashboard from "./Dashboard";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(handleInitialData());
+    this.props.dispatch(handleInitialData())
   }
   render() {
-    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
+    return (
+      <div>
+        {this.props.loading === true
+          ? null
+          : <Dashboard />}
+      </div>
+    )
   }
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps ({ authedUser }) {
   return {
     loading: authedUser === null
-  };
+  }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
