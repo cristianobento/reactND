6e4813cc99f3452c9bc4654d02261258580ceb5e
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-14 00:27:30 -0300

    feat: adding new tweet ui

--- 006-redux/reactnd-chirper-app/src/components/App.js
+++ 006-redux/reactnd-chirper-app/src/components/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { handleInitialData } from '../actions/shared'
-import Dashboard from './Dashboard'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(handleInitialData())
+    this.props.dispatch(handleInitialData());
   }
   render() {
     return (
       <div>
-        {this.props.loading === true
-          ? null
-          : <Dashboard />}
+        <LoadingBar />
+        {this.props.loading === true ? null : <NewTweet />}
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps ({ authedUser }) {
+function mapStateToProps({ authedUser }) {
   return {
     loading: authedUser === null
-  }
+  };
 }
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
--- 006-redux/reactnd-chirper-app/src/components/NewTweet.js
+++ 006-redux/reactnd-chirper-app/src/components/NewTweet.js
@@ -1,62 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class NewTweet extends Component {
   state = {
-    text: '',
-  }
-  handleChange = (e) => {
-    const text = e.target.value
+    text: ""
+  };
+  handleChange = e => {
+    const text = e.target.value;
 
     this.setState(() => ({
       text
-    }))
-  }
-  handleSubmit = (e) => {
-    e.preventDefault()
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
 
-    const { text } = this.state
+    const { text } = this.state;
 
     // todo: Add Tweet to Store
 
-    console.log('New Tweet: ', text)
+    console.log("New Tweet: ", text);
 
     this.setState(() => ({
-      text: ''
-    }))
-  }
+      text: ""
+    }));
+  };
   render() {
-    const { text } = this.state
+    const { text } = this.state;
 
-    {/* todo: Redirect to / if submitted */}
+    {
+      /* todo: Redirect to / if submitted */
+    }
 
-    const tweetLeft = 280 - text.length
+    const tweetLeft = 280 - text.length;
 
     return (
       <div>
-        <h3 className='center'>Compose new Tweet</h3>
-        <form className='new-tweet' onSubmit={this.handleSubmit}>
+        <h3 className="center">Compose new Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
           <textarea
             placeholder="What's happening?"
             value={text}
             onChange={this.handleChange}
-            className='textarea'
+            className="textarea"
             maxLength={280}
           />
-          {tweetLeft <= 100 && (
-            <div className='tweet-length'>
-              {tweetLeft}
-            </div>
-          )}
-          <button
-            className='btn'
-            type='submit'
-            disabled={text === ''}>
-              Submit
+          {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+          <button className="btn" type="submit" disabled={text === ""}>
+            Submit
           </button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default NewTweet
+export default NewTweet;
