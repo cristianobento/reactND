6cfe076627466731682bdd2a1ae5134fb997f520
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-23 18:09:52 -0300

    feat: Adding submit button on AddEntry component

--- 007-react-native/UdaciFitness/components/AddEntry.js
+++ 007-react-native/UdaciFitness/components/AddEntry.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { getMetricMetaInfo } from "../utils/helpers";
+import { View, TouchableOpacity, Text } from "react-native";
+import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -13,67 +21,57 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0
   };
-
   increment = metric => {
     const { max, step } = getMetricMetaInfo(metric);
 
     this.setState(state => {
       const count = state[metric] + step;
+
       return {
         ...state,
         [metric]: count > max ? max : count
       };
     });
   };
-
-  increment = metric => {
-    const { max, step } = getMetricMetaInfo(metric);
-
-    this.setState(state => {
-      const count = state[metric] + step;
-      return {
-        ...state,
-        [metric]: count > max ? max : count
-      };
-    });
-  };
-
-  increment = metric => {
-    const { max, step } = getMetricMetaInfo(metric);
-
-    this.setState(state => {
-      const count = state[metric] + step;
-      return {
-        ...state,
-        [metric]: count > max ? max : count
-      };
-    });
-  };
-
   decrement = metric => {
     this.setState(state => {
       const count = state[metric] - getMetricMetaInfo(metric).step;
+
       return {
         ...state,
         [metric]: count < 0 ? 0 : count
       };
     });
   };
-
   slide = (metric, value) => {
     this.setState(() => ({
       [metric]: value
     }));
   };
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
 
+    // Update Redux
+
+    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
+
+    // Navigate to home
+
+    // Save to "DB"
+
+    // Clear local notification
+  };
   render() {
     const metaInfo = getMetricMetaInfo();
+
     return (
       <View>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
+
           return (
             <View key={key}>
               {getIcon()}
@@ -94,6 +92,7 @@ export default class AddEntry extends Component {
             </View>
           );
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
