4efa1f2d04b12c4cd84efb815ff3c770772c91a7
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-27 15:12:35 -0300

    feat: adding MetricCard component

--- 007-react-native/UdaciFitness/components/MetricCard.js
+++ 007-react-native/UdaciFitness/components/MetricCard.js
@@ -13,9 +13,13 @@ import { fetchCalendarResults } from "../utils/api";
 import UdaciFitnessCalendar from "udacifitness-calendar";
 import { white } from "../utils/colors";
 import DateHeader from "./DateHeader";
-import MetricCard from "./MetricCard.js";
+import MetricCard from "./MetricCard";
+import { AppLoading } from "expo";
 
 class History extends Component {
+  state = {
+    ready: false
+  };
   componentDidMount() {
     const { dispatch } = this.props;
 
@@ -58,6 +62,11 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+
+    if (ready === false) {
+      return <AppLoading />;
+    }
 
     return (
       <UdaciFitnessCalendar
