15b1c32aa931f82938011541ac856a3477c0aa92
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-28 14:49:09 -0300

    feat: adding support to geolocation with expo-permissions

--- 007-react-native/UdaciFitness/components/Live.js
+++ 007-react-native/UdaciFitness/components/Live.js
@@ -8,13 +8,54 @@ import {
 } from "react-native";
 import { Foundation } from "@expo/vector-icons";
 import { purple, white } from "../utils/colors";
+import { Location } from "expo";
+import * as Permissions from "expo-permissions";
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: "denied",
+    status: null,
     direction: ""
   };
+  componentDidMount() {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+
+        this.setState(() => ({ status }));
+      })
+      .catch(error => {
+        console.warn("Error getting Location permission: ", error);
+
+        this.setState(() => ({
+          status: "undetermined"
+        }));
+      });
+  }
+
+  askPermission = () => {};
+
+  setLocation = () => {
+    Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        timeInterval: 1,
+        distanceInterval: 1
+      },
+      ({ coords }) => {
+        const newDirection = calculateDirection(coords.heading);
+        const { direction, bounceValue } = this.state;
+        this.setState(() => ({
+          coords,
+          status: "granted",
+          direction: newDirection
+        }));
+      }
+    );
+  };
+
   render() {
     const { status, coords, direction } = this.state;
 
@@ -50,6 +91,20 @@ export default class Live extends Component {
       <View>
         <Text style={styles.container}>Live</Text>
         <Text>{JSON.stringify(this.state)}</Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>North</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+          </View>
+        </View>
       </View>
     );
   }
