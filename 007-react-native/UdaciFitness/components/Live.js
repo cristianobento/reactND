cee2846e4447d9c6e04ffb21eefc3cb9d41e77f0
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-29 14:49:22 -0300

    feat: updating styles and adding animation for text

--- 007-react-native/UdaciFitness/components/Live.js
+++ 007-react-native/UdaciFitness/components/Live.js
@@ -4,18 +4,21 @@ import {
   Text,
   ActivityIndicator,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  Animated
 } from "react-native";
 import { Foundation } from "@expo/vector-icons";
 import { purple, white } from "../utils/colors";
-import { Location } from "expo";
+import { calculateDirection } from "../utils/helpers";
+import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
 export default class Live extends Component {
   state = {
     coords: null,
     status: null,
-    direction: ""
+    direction: "",
+    bounceValue: new Animated.Value(1)
   };
   componentDidMount() {
     Permissions.getAsync(Permissions.LOCATION)
@@ -47,6 +50,14 @@ export default class Live extends Component {
       ({ coords }) => {
         const newDirection = calculateDirection(coords.heading);
         const { direction, bounceValue } = this.state;
+
+        if (newDirection !== direction) {
+          Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+          ]).start();
+        }
+
         this.setState(() => ({
           coords,
           status: "granted",
@@ -57,7 +68,7 @@ export default class Live extends Component {
   };
 
   render() {
-    const { status, coords, direction } = this.state;
+    const { status, coords, direction, bounceValue } = this.state;
 
     if (status === null) {
       return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -88,21 +99,30 @@ export default class Live extends Component {
     }
 
     return (
-      <View>
-        <Text style={styles.container}>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>North</Text>
+          <Animated.Text
+            style={[
+              styles.direction,
+              { transform: [{ scale: bounceValue }] }
+            ]}
+          >
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Altitude</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {200} feet
+            </Text>
           </View>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Speed</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {300} MPH
+            </Text>
           </View>
         </View>
       </View>
@@ -132,5 +152,38 @@ const styles = StyleSheet.create({
   buttonText: {
     color: white,
     fontSize: 20
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center"
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center"
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5
   }
 });
